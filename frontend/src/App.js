3e5105989b4330545e6621e71b902ee45e73b46a
Vicelmo Alencar <antoniovicelmo@gmail.com>
2024-12-20 16:19:56 -0300

    feat: Adiciona arquivos básicos do React e simplifica App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import Navigation from './components/Navigation';
-import Home from './pages/Home';
-import CreateEbook from './pages/CreateEbook';
-import EbookList from './pages/EbookList';
-import EditEbook from './pages/EditEbook';
 
 const theme = createTheme({
   palette: {
@@ -25,12 +20,8 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
-        <Navigation />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/create" element={<CreateEbook />} />
-          <Route path="/ebooks" element={<EbookList />} />
-          <Route path="/ebooks/:id" element={<EditEbook />} />
+          <Route path="/" element={<div>Escritor - Em construção</div>} />
         </Routes>
       </Router>
     </ThemeProvider>
