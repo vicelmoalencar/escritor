f762890c46bb313e0d41112bfbe48a46df38d6ae
Vicelmo Alencar <antoniovicelmo@gmail.com>
2024-12-20 17:11:10 -0300

    fix: Melhora configurações de build e servidor web

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
